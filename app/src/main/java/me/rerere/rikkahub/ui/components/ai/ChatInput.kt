9e9afd9454fc8dacc8cf5496077cc1959e65dc96
re-ovo <1609403959@qq.com>
2026-08-23 15:48:30 +0800

    chore: 更新依赖
    (cherry picked from commit 02a0c81c00fe402775434186d28c183027535bc4)

--- app/src/main/java/me/rerere/rikkahub/ui/components/ai/ChatInput.kt
+++ app/src/main/java/me/rerere/rikkahub/ui/components/ai/ChatInput.kt
@@ -95,7 +95,7 @@ import me.rerere.hugeicons.HugeIcons
 import me.rerere.hugeicons.stroke.Add01
 import me.rerere.hugeicons.stroke.ArrowUp02
 import me.rerere.hugeicons.stroke.Cancel01
-import me.rerere.hugeicons.stroke.FullScreen
+import me.rerere.hugeicons.stroke.Fullscreen
 import me.rerere.hugeicons.stroke.Zap
 import me.rerere.rikkahub.data.ai.tools.local.LocalToolOption
 import me.rerere.rikkahub.data.ai.tools.local.TodoItem
@@ -733,7 +733,7 @@ private fun TextInputRow(
                         onClick = {
                             isFullScreen = !isFullScreen
                         }) {
-                        Icon(HugeIcons.FullScreen, null)
+                        Icon(HugeIcons.Fullscreen, null)
                     }
                 }
             },
